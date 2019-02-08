ceca8958626a599009735c60ad1e2d76b366ce4e
pinfinite <grewalakksh@outlook.com>
2019-02-08 15:37:11 +0530

    initial commit

---
+++ src/main/java/com/pinfinite/prengine/PrengineApplication.java
@@ -0,0 +1,14 @@
+package com.pinfinite.prengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrengineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrengineApplication.class, args);
+	}
+
+}
+
